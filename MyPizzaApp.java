54d350deee2a3ee11c5ceffffe19b8a67ff83d3a
SpurthiSetty <spurthi.setty@gmail.com>
2024-11-21 14:37:07 -0500

    added print statements

--- MyPizzaApp.java
+++ MyPizzaApp.java
@@ -2,13 +2,15 @@
 // Main function to order pizzas
 public class MyPizzaApp {
     public static void main(String[] args) {
-        // Order Cheese Pizza from NY Pizza Store
+        // Order Veggie Pizza from NY Pizza Store
         PizzaStore nyPizzaStore = new NYPizzaStore();
-        Pizza nyPizza = nyPizzaStore.orderPizza("cheese");
+        Pizza nyPizza = nyPizzaStore.orderPizza("veggie");
         System.out.println("Ordered a " + nyPizza.getName() + "\n");
+        System.out.println(nyPizza);
 
         PizzaStore chicagoPizzaStore = new ChicagoPizzaStore();
-        Pizza chicagoPizza = chicagoPizzaStore.orderPizza("veggie");
+        Pizza chicagoPizza = chicagoPizzaStore.orderPizza("cheese");
         System.out.println("Ordered a " + chicagoPizza.getName() + "\n");
+        System.out.println(chicagoPizza);
     }
 }
